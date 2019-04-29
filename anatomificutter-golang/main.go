73065d569847638f734d354331249874d78090c0
Aramis <aramishm@gmail.com>
2019-04-28 23:29:50 -0300

    Program generating images

--- anatomificutter-golang/main.go
+++ anatomificutter-golang/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/color"
 	"image/png"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -19,78 +19,108 @@ func printUsage() {
 
 USAGE
 
-param 1 - Project Name
+param 1 - Input directory
 
-param 2 - Input directory
-
-param 3 - Output directory
-\n\n`)
+param 2 - Coronal cut depth
+`)
 	os.Exit(0)
 }
 
-func getFiles() {
+// GetImageLen - Get image length from a file
+func GetImageLen(filePath os.FileInfo) int {
+	tmpFile, err := os.Open(filePath.Name())
+	if err != nil {
+		fmt.Println("Image file couldn't be opened.")
+		os.Exit(1)
+	}
+	defer tmpFile.Close()
+	imageToGetLenth, _, err := image.Decode(tmpFile)
+	lineLen := imageToGetLenth.Bounds().Dx()
 
+	return lineLen
 }
 
-func main() {
+// GetFilesFromExtension
+func GetFilesFromExtension(extension string, path string) (int, []os.FileInfo) {
 
-	// get arguments
-	argsWithoutProg := os.Args[1:]
-
-	if len(argsWithoutProg) < 2 {
-		printUsage()
-	}
-
-	files, err := ioutil.ReadDir(argsWithoutProg[1])
+	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// get number of png files
+	var theFiles []os.FileInfo
+	counter := 0
 	for _, f := range files {
-		fileName := f.Name()
-		name := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+		fullPathFile := f.Name()
+		fileExt := filepath.Ext(fullPathFile)
 
-		outputDir := "./"
-		if len(argsWithoutProg) > 1 {
-			outputDir = argsWithoutProg[2]
+		// only .png files allowed
+		if fileExt != "."+extension {
+			continue
+		} else {
+			theFiles = append(theFiles, f)
+			counter++
 		}
-
-		file, err := os.Create(outputDir + "/" + name + ".xml") // Truncates if file already exists, be careful!
-		if err != nil {
-			log.Fatalf("failed creating file: %s", err)
-			return
-		}
-
-		file.WriteString(`aio`)
-		file.Close()
 	}
 
-	width := 300
-	height := 300
+	return counter, theFiles
+}
 
-	upLeft := image.Point{0, 0}
-	lowRight := image.Point{width, height}
+func main() {
 
-	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+	// Get arguments
+	argsWithoutProg := os.Args[1:]
 
-	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
-	cyan := color.RGBA{100, 200, 200, 0xff}
+	// Get the parameters
+	if len(argsWithoutProg) < 2 {
+		printUsage()
+	}
 
-	// Set color for each pixel.
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			switch {
-			case x < width/2 && y < height/2: // upper left quadrant
-				img.Set(x, y, cyan)
-			case x >= width/2 && y >= height/2: // lower right quadrant
-				img.Set(x, y, color.White)
-			default:
-				// Use zero value.
+	cutDepth, _ := strconv.ParseInt(argsWithoutProg[1], 10, 64)
+
+	counter, theFiles := GetFilesFromExtension("png", argsWithoutProg[0])
+
+	if counter > 0 {
+		// Get image length
+		//lineHeight := 0
+		lineLen := GetImageLen(theFiles[0])
+
+		width := lineLen
+		height := counter
+
+		upLeft := image.Point{0, 0}
+		lowRight := image.Point{width, height}
+		img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+		for y, f := range theFiles {
+			fullPathFile := f.Name()
+			fileExt := filepath.Ext(fullPathFile)
+			fileNameOnly := strings.TrimSuffix(fullPathFile, filepath.Ext(fullPathFile))
+
+			// only .png files allowed
+			if fileExt != ".png" {
+				continue
 			}
+
+			imgfile, err := os.Open(fullPathFile)
+			if err != nil {
+				fmt.Println("Image file couldn't be opened.")
+				os.Exit(1)
+			}
+			defer imgfile.Close()
+
+			imgFromFile, _, err := image.Decode(imgfile)
+
+			// Set color for each pixel.
+			for x := 0; x < width; x++ {
+				color := imgFromFile.At(x, (int)(cutDepth))
+				img.Set(x, y, color)
+			}
+
+			// Encode as PNG.
+			f, _ := os.Create("out-" + fileNameOnly + ".png")
+			png.Encode(f, img)
 		}
 	}
-
-	// Encode as PNG.
-	f, _ := os.Create("image.png")
-	png.Encode(f, img)
 }
